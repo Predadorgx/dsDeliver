662365fa85f651e27d4ed58d51cb5acc5756d62e
unknown <erickgamersilva@hotmail.com>
2021-01-08 11:43:39 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/dsDeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsDeliver/services/OrderService.java
@@ -42,4 +42,11 @@ public class OrderService {
 		order = repository.save(order);
 		return new OrderDTO(order);
 	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DERIVERED);
+		return new OrderDTO(order);
+	}
 }
