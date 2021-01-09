1a42c216ab30693dcdca4c80bcb11fd31d88ac6a
unknown <erickgamersilva@hotmail.com>
2021-01-09 11:45:09 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,11 +2,14 @@ import './styles.css';
 import StepsHeader from "./StepsHeader";
 import ProductsList from './ProductsList';
 import { useEffect, useState } from 'react';
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import { decodedTextSpanIntersectsWith } from 'typescript';
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
     console.log(products);
 
@@ -18,6 +21,7 @@ function Orders() {
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}></OrderLocation>
         </div>
     )
 
